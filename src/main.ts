d6d00234ee65c86d3aaa96fe3d0c1128233266a4
walidchb <walidchebbab2001@gmail.com>
2026-07-20 14:51:02 +0100

    make CORS origins configurable via CORS_ORIGINS env var

--- src/main.ts
+++ src/main.ts
@@ -24,8 +24,11 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
 
   // Enable CORS
+  const corsOrigins = process.env.CORS_ORIGINS
+    ? process.env.CORS_ORIGINS.split(',')
+    : ['http://localhost:3002', 'http://localhost:3001', 'http://localhost:3000'];
   app.enableCors({
-    origin: ['http://localhost:3002', 'http://localhost:3001', 'http://localhost:3000'],
+    origin: corsOrigins,
     credentials: true,
   });
 
